b1c46d9f5d9828b47c15b95c36bb3e90d3ec3de2
Brent Noorda <brent.noorda@gmail.com>
2016-04-17 10:18:23 -0700

    learn about logging

--- catchylink.go
+++ catchylink.go
@@ -3,6 +3,8 @@ package catchylink
 import (
     "fmt"
     "net/http"
+    "google.golang.org/appengine"
+    "google.golang.org/appengine/log"
 )
 
 func init() {
@@ -10,5 +12,11 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Catchylink3, world!<br/>Path:" + r.URL.Path + "<br/>RawPath:" + r.URL.RawPath + "<br/>RawQuery:" + r.URL.RawQuery)
+    ctx := appengine.NewContext(r)
+    log.Infof(ctx,"Catchylink3, world!<br/>Path:\"" + r.URL.Path + "\"  RawPath:\"" + r.URL.RawPath + "\"  RawQuery:\"" + r.URL.RawQuery + "\"")
+    if r.URL.Path == "/" {
+        fmt.Fprintf(w, "Return index.html")
+    } else {
+        fmt.Fprint(w, "Catchylink3, world!<br/>Path:" + r.URL.Path + "<br/>RawPath:" + r.URL.RawPath + "<br/>RawQuery:" + r.URL.RawQuery)
+    }
 }
--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def open_browser_windows():
     shell("open http://localhost:8000/ &")
 
 
-retCode = shell("go build")
+retCode = shell("goapp build")
 if retCode == 0:
     open_browser_windows()
     shell("goapp serve ./")
