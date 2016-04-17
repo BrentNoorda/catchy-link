86d833a2859f411f8d49c8d4771a997bddb65cd3
Brent Noorda <brent.noorda@gmail.com>
2016-04-17 11:47:50 -0700

    read from real index.html page

--- catchylink.go
+++ catchylink.go
@@ -2,6 +2,7 @@ package catchylink
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
     "google.golang.org/appengine"
     "google.golang.org/appengine/log"
@@ -11,11 +12,20 @@ func init() {
     http.HandleFunc("/", handler)
 }
 
+func homepage(w http.ResponseWriter, r *http.Request) {
+    text, err := ioutil.ReadFile("web/index.html")
+    if err != nil {
+        ctx := appengine.NewContext(r)
+        log.Errorf(ctx,"Could not read homepage")
+    }
+    fmt.Fprint(w,string(text))
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     ctx := appengine.NewContext(r)
     log.Infof(ctx,"Catchylink3, world!<br/>Path:\"" + r.URL.Path + "\"  RawPath:\"" + r.URL.RawPath + "\"  RawQuery:\"" + r.URL.RawQuery + "\"")
     if r.URL.Path == "/" {
-        fmt.Fprintf(w, "Return index.html")
+        homepage(w,r)
     } else {
         fmt.Fprint(w, "Catchylink3, world!<br/>Path:" + r.URL.Path + "<br/>RawPath:" + r.URL.RawPath + "<br/>RawQuery:" + r.URL.RawQuery)
     }
