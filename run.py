bdc781e5b8aec1fc3add94d98b3106c4fbf60317
Brent Noorda <brent.noorda@gmail.com>
2016-04-17 20:32:43 -0700

    get a simple page up, with a form, and form validation Merge branch 'local-master'

--- run.py
+++ run.py
@@ -10,11 +10,11 @@ def shell(s):
     return os.system(s)
 
 def open_browser_windows():
-    shell("open http://localhost:8080/ &")
     shell("open http://localhost:8000/ &")
+    shell("open http://localhost:8080/ &")
 
 
-retCode = shell("go build")
+retCode = shell("goapp build")
 if retCode == 0:
     open_browser_windows()
     shell("goapp serve ./")
